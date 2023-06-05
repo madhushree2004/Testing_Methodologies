3fa632ab819e653853e06164dd5a6f51ba9bb0ee
madhushree2004 <113434793+madhushree2004@users.noreply.github.com>
2023-06-05 19:52:34 +0530

    Add files via upload

---
+++ day5/src/main/java/com/example/demo/day5/day5_4.java
@@ -0,0 +1,33 @@
+package com.example.demo.day5;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class day5_4 {
+
+	public static void main(String[] args) 
+	{
+		WebDriverManager.edgedriver().setup();
+	      WebDriver driver = new EdgeDriver();
+	      driver.get("https://j2store.net/free/");
+	      driver.manage().window().maximize();
+	      JavascriptExecutor js = (JavascriptExecutor)driver;
+			js.executeScript("window.scrollBy(0,3800)", "");
+			driver.findElement(By.xpath("//*[@id=\"Mod112\"]/div/div/ul/li[1]/h4/a")).click();
+	      List<WebElement> m = driver.findElements(By.className("product-title"));
+	      // iterate over list
+	      for(int i = 0; i< m.size(); i++) {
+	         //obtain text
+	         String s = m.get(i).getText();
+	         System.out.println("Text is: " + s);
+	      }
+	      driver.quit();
+	}
+}
